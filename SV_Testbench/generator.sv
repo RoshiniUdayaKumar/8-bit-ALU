1745cb47aada363dfc0511ad998fe91a9b31f945
Roshini Udaya <133715179+RoshiniUdayaKumar@users.noreply.github.com>
2025-08-18 10:43:28 +0530

    Update generator.sv

--- SV_Testbench/generator.sv
+++ SV_Testbench/generator.sv
@@ -1,1 +1,23 @@
-
+class generator;
+  
+  //create transaction instance
+  alu_transaction txn;
+  
+  //create a mailbox-->we use the same mailbox throughout
+  mailbox gen2drv;
+  
+  //constructor 
+  function new(mailbox gen2drv);
+    this.gen2drv=gen2drv;   
+  endfunction
+  
+  task run();
+      repeat(10) begin 
+        txn=new();
+      txn.randomize();
+      $display("Generated random values of a:%0d, b:%0d, opcode:%0b", txn.a,txn.b,txn.opcode);
+      gen2drv.put(txn);  //put to mailbox
+    end
+   endtask
+endclass
+  
